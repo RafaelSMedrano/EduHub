1a0d0f56b56db6dd6b48955e4c7dd632f011b5c9
RafaelSMedrano <rafael.smedrano@gmail.com>
2024-02-11 14:09:30 +0000

    fixed untracked files

--- protected/vendor/npm-asset/humhub-prosemirror-richtext/src/editor/core/plugins/blockquote/input-rules.js
+++
@@ -1,8 +0,0 @@
-import {wrappingInputRule} from "prosemirror-inputrules";
-
-// : (NodeType) → InputRule
-// Given a blockquote node type, returns an input rule that turns `"> "`
-// at the start of a textblock into a blockquote.
-const blockquoteRule = (schema) => wrappingInputRule(/^\s*>\s$/, schema.nodes.blockquote);
-
-export {blockquoteRule};
